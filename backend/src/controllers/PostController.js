a080de5138c7446c2007e8da897ff3e17f66e216
Marcelo Ratton <rattones@marceloratton.com>
2019-06-12 01:28:04 -0300

    keep going ..

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,11 +1,19 @@
-// const Post = require('./models/Post')
+const Post = require('../models/Post')
 
 module.exports = {
     async index(req, res) {
-        return 
+        const posts = await Post.find().sort('-createdAt')
+        return res.json(posts)
     },
 
     async store(req, res) {
-        return res.json({ ok: true })
+        const { author, place, desctiption, hashtags } = req.body
+        const { filename: image } = req.file
+
+        const post = await Post.create({
+            author, place, desctiption, hashtags, image
+        })
+
+        return res.json(post)
     },
 }
--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -12,7 +12,7 @@ const PostSchema = new mongoose.Schema({
     }
 },
 {
-    timestamp: true
+    timestamps: true
 })
 
 module.exports = mongoose.model('Post', PostSchema)
